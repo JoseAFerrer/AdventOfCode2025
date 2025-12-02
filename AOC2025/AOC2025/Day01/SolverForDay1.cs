05c5c3e583831087f0b80e2c87ecc0b39bfa7a3d
JoseAFerrer <seriamente.fs@gmail.com>
2025-12-02 22:04:53 +0100

    Refactor and apply code suggestions

--- AOC2025/AOC2025/Day01/SolverForDay1.cs
+++ AOC2025/AOC2025/Day01/SolverForDay1.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace AOC2025.Day01;
 
-public class SolverForDay1
+public static class SolverForDay1
 {
     public static void Solve()
     {
-        var baseLines = FileHelpers.ReadInputLines(1, FileHelpers.Input.Sample);
+        var baseLines = FileHelpers.ReadInputLines(1, FileHelpers.Input.Real);
         var operations = ConvertToOperations(baseLines);
 
         var start = 50;
@@ -17,34 +13,13 @@ public class SolverForDay1
         
         foreach (var operation in operations)
         {
-            var hundreds = Math.Abs(operation) / modulo;
-            if (hundreds >= 1)
-            {
-                Console.WriteLine("hundreds");
-                counter += hundreds;
-            }
-            
-            var reducedStart = start % modulo;
-            var reducedOperation = operation % modulo;
-            var reducedResult = reducedStart + reducedOperation;
-            var wentTooHigh = ((reducedStart > 0) && reducedResult > 100) ||
-                              ((reducedStart < 0) && reducedResult > 0) ;
-            var wentTooLow =  ((reducedStart > 0) && reducedResult < 0) ||
-                              ((reducedStart < 0) && reducedResult < -100) ;
-            if (wentTooHigh || wentTooLow)
-            {
-                Console.WriteLine("hello");
-                counter++;
-            }
-            
+            counter = CountTimesModuloIsContainedInOperation(operation, modulo, counter);
+
+            counter = CountIfOperationChangedRangeWithModulo(start, modulo, operation, counter);
+
             var result = start + operation;
 
-            if (result % 100 == 0)
-            {
-                counter++;
-            }
-            
-            Console.WriteLine(result);
+            counter = CheckIfResultIsModuloZero(result, counter);
             start = result;
         }
 
@@ -52,11 +27,47 @@ public class SolverForDay1
 
     }
 
+    private static int CountTimesModuloIsContainedInOperation(int operation, int modulo, int counter)
+    {
+        var hundreds = Math.Abs(operation) / modulo;
+        if (hundreds < 1) return counter;
+        
+        counter += hundreds;
+        return counter;
+    }
+
+    private static int CountIfOperationChangedRangeWithModulo(int start, int modulo, int operation, int counter)
+    {
+        var reducedStart = start % modulo;
+        var reducedOperation = operation % modulo;
+        var reducedResult = reducedStart + reducedOperation;
+        var wentTooHigh = (reducedStart > 0 && reducedResult > 100) ||
+                          (reducedStart < 0 && reducedResult > 0) ;
+        var wentTooLow =  (reducedStart > 0 && reducedResult < 0) ||
+                          (reducedStart < 0 && reducedResult < -100) ;
+        if (wentTooHigh || wentTooLow)
+        {
+            counter++;
+        }
+
+        return counter;
+    }
+
+    private static int CheckIfResultIsModuloZero(int result, int counter)
+    {
+        if (result % 100 == 0)
+        {
+            counter++;
+        }
+
+        return counter;
+    }
+
     private static List<int> ConvertToOperations(string[] baseLines)
     {
         var operations = baseLines.Select(x =>
         {
-            var isAdding = x.StartsWith("R");
+            var isAdding = x.StartsWith('R');
             var number = int.Parse(x[1..]);
             return isAdding
                 ? number
