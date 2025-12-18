ae1bbea1c44d13fe1f9912999e4183474b201299
JoseAFerrer <seriamente.fs@gmail.com>
2025-12-18 18:44:24 +0100

    Calculate distances

--- AOC2025/AOC2025/Day08/Day08.cs
+++ AOC2025/AOC2025/Day08/Day08.cs
@@ -9,11 +9,26 @@ public class Day08
         var input = FileHelpers.ReadInputLines(8, FileHelpers.Input.Sample);
         var circuits = input
             .Select(line => new JBox(line))
-            .Select(x => new Circuit([x]));
+            .Select(x => new Circuit([x]))
+            .ToArray();
+        
+        CalculateDistancesToClosest(circuits);
 
+        var close = circuits.Where(x => x.DistanceToClosest < 320);
+    }
+
+    private static void CalculateDistancesToClosest(Circuit[] circuits)
+    {
         foreach (var circuit in circuits)
         {
+            var distancesAndIds = circuits
+                .Where(x => x.Id != circuit.Id)
+                .Select(x => new { x.Id, distance = x.DistanceTo(circuit)})
+                .OrderBy(x => x.distance)
+                .ToArray();
+            
+            circuit.ClosestCircuitId = distancesAndIds.First().Id;
+            circuit.DistanceToClosest = distancesAndIds.First().distance;
         }
-        
     }
 }
